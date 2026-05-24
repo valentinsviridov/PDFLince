1f1c688cac3a8a1f6c4ad461d32b13b2334fa977
Renata Suba <suba.renata.kinga@gmail.com>
2026-05-24 23:25:36 +0200

    remove unused imports

--- src/app/(es)/apoya/page.tsx
+++ src/app/(es)/apoya/page.tsx
@@ -1,7 +1,3 @@
-import NavMenu from "../../../components/NavMenu";
-import Footer from "../../../components/Footer";
-import FotoLinceBanner from "../../../components/FotoLinceBanner";
-import { LocaleProvider } from "../../../i18n/LocaleProvider";
 import { DEFAULT_LOCALE } from "../../../i18n/config";
 import { SupportPageContent } from "../../../components/SupportPageContent";
 import { createSupportMetadata } from "../../(helpers)/locale-metadata";
