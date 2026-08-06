f5113844fdebd772e6ef51ba4674b44e5ca3ad5f
Valentin Sviridov <valentin.s.sviridov@gmail.com>
2026-08-06 21:16:50 +0200

    feat: change local playwright port

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: process.env.CI ? 'http://localhost:3000' : 'http://localhost:3000',
+        baseURL: process.env.CI ? 'http://localhost:3000' : 'http://localhost:65000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -34,8 +34,8 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'bun run dev',
-        url: 'http://localhost:3000',
+        command: 'PORT=65000 bun run dev',
+        url: 'http://localhost:65000',
         reuseExistingServer: !process.env.CI,
         stdout: 'ignore',
         stderr: 'pipe',
